3a14b66f684e3c0fe3a6c2198ea4d14b4e3740c2
Karl O. Feger <kfeger@yahoo.de>
2017-12-13 23:04:47 +0100

    Initial commit

---
+++ WeatheStationFunctions.ino
@@ -0,0 +1,250 @@
+//Wird gerufen,wenn das bisherige WLAN
+//nicht mehr vorhanden ist
+void configModeCallback (WiFiManager *ThisWiFiManager) {
+  display.clear();
+  display.drawString(64, 0, "Nicht gefunden.");
+  display.drawString(64, 14, "Bitte mit WLAN");
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //Serial.println(WiFi.softAP(ssid));
+  //if you used auto generated SSID, print it
+  display.drawString(64, 28, ThisWiFiManager->getConfigPortalSSID());
+  display.drawString(64, 42, "konfigurieren");
+  display.display();
+  Serial.println(ThisWiFiManager->getConfigPortalSSID());
+}
+
+void updateData(OLEDDisplay *display) {
+  drawProgress(display, 25, "Zeit holen...");
+  worldClockClient.updateTime();
+  drawProgress(display, 50, "Wetter jetzt holen...");
+  wunderground.updateConditions(WUNDERGRROUND_API_KEY, WUNDERGRROUND_LANGUAGE, WUNDERGROUND_COUNTRY, WUNDERGROUND_CITY);
+  drawProgress(display, 75, "Vorhersage holen...");
+  wunderground.updateForecast(WUNDERGRROUND_API_KEY, WUNDERGRROUND_LANGUAGE, WUNDERGROUND_COUNTRY, WUNDERGROUND_CITY);
+  lastUpdate = timeClient.getFormattedTime();
+  drawProgress(display, 100, "Fertig...");
+  readyForUpdate = false;
+  delay(1000);
+}
+
+void drawProgress(OLEDDisplay *display, int percentage, String label) {
+  display->clear();
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(64, 10, label);
+  display->drawProgressBar(10, 28, 108, 12, percentage);
+  display->display();
+}
+
+void drawClock(OLEDDisplay *display, int x, int y, int timeZoneIndex, String city, const char* icon) {
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(x + 60, y + 5, city);
+  display->setFont(Crushed_Plain_36);
+  display->drawXbm(x, y, 60, 60, icon);
+  display->drawString(x + 60, y + 15, worldClockClient.getHours(timeZoneIndex) + ":" + worldClockClient.getMinutes(timeZoneIndex));
+
+}
+
+void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  drawClock(display, x, y, 0, "New York",  new_york_bits);
+}
+
+void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  drawClock(display, x, y, 1, "London",  london_bits);
+  if(!SwitchKMdone) {
+    SwitchKMdone = true;
+    if(SwitchKM)
+      SwitchKM = false;
+    else
+      SwitchKM = true;
+  }
+}
+
+void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  drawClock(display, x, y, 2, "München",  munich);
+  CalcDone = false; //darf nicht ober- oder unterhalb des betroffenen Frames stehen!!
+  SwitchKMdone = false;
+}
+
+void drawFrame4(OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  drawClock(display, x, y, 3, "Sydney",  sydney_bits);
+}
+
+void drawCurrentDetails(OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(32 + x, 0 + y, "Feuchte");
+  display->drawString(96 + x, 0 + y, "Druck");
+  display->drawString(32 + x, 28 + y, "Niederschl.");
+  display->drawString(96 + x, 28 + y, "heute in");
+
+  display->setFont(ArialMT_Plain_16);
+  display->drawString(32 + x, 10 + y, wunderground.getHumidity());
+  display->drawString(96 + x, 10 + y, wunderground.getPressure());
+  display->drawString(32 + x, 38 + y, wunderground.getPrecipitationToday());
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(96 + x, 38 + y, "München");
+}
+
+
+void drawPressure (OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  // Start a pressure measurement:
+  // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  ui.disableIndicator();
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(32 + x, 28 + y, "Temperatur");
+  display->drawString(96 + x, 0 + y, "Druck");
+  display->drawString(32 + x, 0 + y, "Feuchte");
+  display->drawString(96 + x, 28 + y, "hier und");
+
+  display->setFont(ArialMT_Plain_16);
+  String temp = String(LuftTemp, 1);
+  temp += "°C";
+  display->drawString(32 + x, 38 + y, temp);  //Temperatur
+  dtostrf(p0, 10, 0, PBuffer);
+  temp = PBuffer; // + " hPa rel.";
+  temp.trim();
+  temp += "mb";
+  display->drawString(96 + x, 10 + y, temp);  //Druck
+  temp = String(Humid, 0);
+  temp += "%";
+  display->drawString(32 + x, 10 + y, temp);  //Feuchte
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(96 + x, 38 + y, "jetzt");
+
+  CalcDone = true;
+}
+
+void drawHohenrhein(OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  display->setFont(Dialog_bold_12);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(64 + x, 16 + y, "Und heute");
+  display->drawString(64 + x, 32 + y, "zuhause...");
+}
+
+void drawGoodBye(OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  //display->setFont(Luckiest_Guy_14);
+  ui.disableIndicator();
+  display->setFont(URW_Bookman_L_Light_Italic_16);
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->drawString(64 + x, 0 + y, "Du wirst");
+  display->drawString(64 + x, 20 + y, "uns fehlen!");
+  display->drawString(64 + x, 40 + y, "AG InfoSic ITPLR");
+}
+
+void drawStones(OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  //display->setFont(Luckiest_Guy_14);
+  ui.disableIndicator();
+  display->drawXbm((DISPLAY_WIDTH / 2 - Stones_width / 2) + x, (DISPLAY_HEIGHT / 2 - Stones_height / 2) + y, Stones_width, Stones_height, Stones);
+}
+
+void drawCurrentWeather(OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  display->setFont(ArialMT_Plain_10);
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->drawString(60 + x, 5 + y, wunderground.getWeatherText());
+
+  display->setFont(ArialMT_Plain_24);
+  String temp = wunderground.getCurrentTemp() + "°C";
+  display->drawString(56 + x, 15 + y, temp);
+  int tempWidth = display->getStringWidth(temp);
+
+  display->setFont(Meteocons_Plain_42);
+  String weatherIcon = wunderground.getTodayIcon();
+  int weatherIconWidth = display->getStringWidth(weatherIcon);
+  display->drawString(32 + x - weatherIconWidth / 2, 05 + y, weatherIcon);
+}
+
+void drawForecastDetails(OLEDDisplay * display, int x, int y, int dayIndex) {
+  ui.disableIndicator();
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_10);
+  String day = wunderground.getForecastTitle(dayIndex).substring(0, 3);
+  day.toUpperCase();
+  display->drawString(x + 20, y, day);
+
+  display->setFont(Meteocons_Plain_21);
+  display->drawString(x + 20, y + 12, wunderground.getForecastIcon(dayIndex));
+
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(x + 20, y + 34, wunderground.getForecastLowTemp(dayIndex) + "|" + wunderground.getForecastHighTemp(dayIndex));
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+}
+
+void drawHeaderOverlay(OLEDDisplay * display, OLEDDisplayUiState * state) {
+  ui.disableIndicator();
+  display->setColor(WHITE);
+  display->setFont(ArialMT_Plain_10);
+  String time = timeClient.getFormattedTime().substring(0, 5);
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->drawString(0, 54, time);
+  display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  String temp = wunderground.getCurrentTemp() + "°C";
+  display->drawString(128, 54, temp);
+  display->drawHorizontalLine(0, 52, 128);
+}
+
+void drawForecast(OLEDDisplay * display, OLEDDisplayUiState * state, int16_t x, int16_t y) {
+  ui.disableIndicator();
+  drawForecastDetails(display, x, y, 0);
+  drawForecastDetails(display, x + 44, y, 2);
+  drawForecastDetails(display, x + 88, y, 4);
+}
+
+void calcPress(void) {
+
+  // Retrieve the completed pressure measurement:
+  // Note that the measurement is stored in the variable P.
+  // Note also that the function requires the previous temperature measurement (T).
+  // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+  // Function returns 1 if successful, 0 if failure.
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      Serial.print("BH180: Temperatur ");
+      Serial.print(T, 2);
+      Serial.print(" C, Abs. Luftdruck: ");
+    }
+    status = pressure.startPressure(3);
+    delay(status);
+    status = pressure.getPressure(P, T);
+    if (status != 0)
+    {
+      Serial.print(P, 2);
+      Serial.print(" hPa, ");
+    }
+    // The pressure sensor returns abolute pressure, which varies with altitude.
+    // To remove the effects of altitude, use the sealevel function and your current altitude.
+    // This number is commonly used in weather reports.
+    // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+    // Result: p0 = sea-level compensated pressure in mb
+    p0 = pressure.sealevel(P, ALTITUDE);
+    Serial.print("rel. Luftdruck: ");
+    Serial.print(p0, 2);
+    Serial.println(" hPa");
+    Humid = luftfeuchte.readHumidity();
+    LuftTemp = luftfeuchte.readTemperature();
+    Serial.print("HTU21: Temperatur ");
+    Serial.print(LuftTemp);
+    Serial.print(" C, rel. Feuchte ");
+    Serial.print(Humid);
+    Serial.println(" %");
+  }
+}
+
---
+++ WeatherStationAndy.ino
@@ -0,0 +1,287 @@
+/**The MIT License (MIT)
+  See more at http://blog.squix.ch
+
+  Entwickeltauf Arduino IDE 1.6.9 mit
+  Board-Definition von https://github.com/esp8266/Arduino
+  eigene Fonts machen mit http://oleddisplay.squix.ch/
+*/
+
+#include <string.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+//needed for WiFi-Manager library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <JsonListener.h>
+#include "SSD1306.h"            //https://github.com/squix78/esp8266-oled-ssd1306
+#include "OLEDDisplayUi.h"      //dto.
+#include "Wire.h"
+#include "WorldClockClient.h"   //https://github.com/skx/esp8266/blob/master/d1-weather-station/WorldClockClient.cpp
+#include "WundergroundClient.h" //https://github.com/squix78/esp8266-weather-station
+#include "TimeClient.h"
+#include "WeatherStationFonts.h"
+#include "icons.h"
+#include "fonts.h"
+#include "images.h"
+#include <SFE_BMP180.h>       //https://github.com/sparkfun/BMP180_Breakout
+#include "SparkFunHTU21D.h"   //https://github.com/sparkfun/SparkFun_HTU21D_Breakout_Arduino_Library
+
+ADC_MODE(ADC_VCC);
+
+
+/***************************
+   Begin Settings
+ **************************/
+
+// Setup
+const int UPDATE_INTERVAL_SECS = 10 * 60; // Update every 10 minutes
+
+// Wunderground Settings
+const boolean IS_METRIC = true;
+const String WUNDERGRROUND_API_KEY = "738640203c8f39bc";  //kfeger@web.de mit PW SoPe2016
+const String WUNDERGRROUND_LANGUAGE = "DL";
+const String WUNDERGROUND_COUNTRY = "Germany";
+const String WUNDERGROUND_CITY = "Munich";
+
+// TimeClient settings
+const float UTC_OFFSET = 2;
+
+// Display Settings
+const int I2C_DISPLAY_ADDRESS = 0x3c;
+const int SDA_PIN = 0;
+const int SDC_PIN = 2;
+
+SSD1306  display(I2C_DISPLAY_ADDRESS, SDA_PIN, SDC_PIN);
+OLEDDisplayUi ui     ( &display );
+
+// Barometer Settings
+SFE_BMP180 pressure;
+#define ALTITUDE 519.0 // Höhe München lt. Wikipedia
+double T = 0, P = 0, p0 = 0, a = 0;
+char PBuffer[15];
+char status = 0;
+bool CalcDone = false, SwitchKM = false, SwitchKMdone = false;
+
+//Feuchte Settings
+HTU21D luftfeuchte;
+float Humid = 0;
+float LuftTemp = 0;
+unsigned long LastPress = 0;
+
+//Voltage
+String VoltString;
+int Volt = 0;
+uint32_t lastVolt = 0;
+#define VOLT_LOW_LIMIT 2350 //in mV
+
+/***************************
+   End Settings
+ **************************/
+String timeZoneIds [] = {"America/New_York", "Europe/London", "Europe/Berlin", "Australia/Sydney"};
+WorldClockClient worldClockClient("de", "Germany", "E, dd. MMMMM yyyy", 4, timeZoneIds);
+
+
+// flag changed in the ticker function every 10 minutes
+bool readyForUpdate = false;
+
+String lastUpdate = "--";
+
+TimeClient timeClient(UTC_OFFSET);
+Ticker ticker;
+
+// Set to false, if you prefere imperial/inches, Fahrenheit
+WundergroundClient wunderground(IS_METRIC);
+
+
+void setReadyForWeatherUpdate() {
+  Serial.println("Setting readyForUpdate to true");
+  readyForUpdate = true;
+}
+
+// this array keeps function pointers to all frames
+// frames are the single views that slide from right to left
+//FrameCallback frames[] = { drawFrame1, drawFrame2, drawFrame3, drawFrame4, drawCurrentWeather, drawForecast, drawPressure};
+//int numberOfFrames = 7;
+FrameCallback frames[] = { drawFrame2, drawFrame1, drawFrame3, drawCurrentWeather, drawForecast, drawCurrentDetails, drawPressure, drawStones, drawGoodBye};
+int numberOfFrames = 9;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  // initialize dispaly
+  display.init();
+  display.clear();
+  display.display();
+  // Gedaddel für einen netten Start
+  int Volt = ESP.getVcc();
+  Volt = (Volt / 100) * 100;
+  VoltString = Volt / 1000;
+  VoltString += ",";
+  VoltString += Volt - (Volt / 1000) * 1000;
+  VoltString.remove(3, 2);  //eine Nachkommastelle reicht
+  VoltString += "V";
+  if (Volt < VOLT_LOW_LIMIT)
+    VoltString += " low!";
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_24);
+  //String ssidShow = ssid;
+  //textWidth = display.getStringWidth(ssidShow);
+  display.drawString(64, 0, "LiPo-Akku");
+  display.drawString(64, 30, VoltString);
+  display.display();
+  delay(1000);
+
+  display.clear();
+  display.setFont(URW_Chancery_L_Medium_Italic_24);
+  
+  display.drawString(64, 0, "Andy Mück");
+  display.drawString(64, 30, "IoT-Spezial");
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.display();
+  delay(2000);
+
+  //display.flipScreenVertically();
+  display.setFont(Dialog_bold_12);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setContrast(255);
+
+  if (pressure.begin())
+    Serial.println("BMP180 init erfolgreich");
+  else
+  {
+    Serial.println("BMP180 init Fehler\n\n");
+    delay(5000); // Pause forever.
+  }
+  luftfeuchte.begin();
+
+  randomSeed(ESP.getVcc());
+  int Pick = (int)random(0, 8);
+  Serial.print(F("Pick = "));
+  Serial.println(Pick);
+  display.clear();
+  switch (Pick) {
+    case 0:
+      display.drawXbm(DISPLAY_WIDTH / 2 - Sachsen_Width / 2, DISPLAY_HEIGHT / 2 - Sachsen_Height / 2, Sachsen_Width, Sachsen_Height, Sachsen);
+      break;
+    case 1:
+      display.drawXbm(DISPLAY_WIDTH / 2 - Duffy_Width / 2, DISPLAY_HEIGHT / 2 - Duffy_Height / 2, Duffy_Width, Duffy_Height, Duffy);
+      break;
+    case 2:
+      display.drawXbm(DISPLAY_WIDTH / 2 - munich_width / 2, DISPLAY_HEIGHT / 2 - munich_height / 2, munich_width, munich_height, munich);
+      break;
+    case 3:
+      display.drawXbm(DISPLAY_WIDTH / 2 - BulbOff_Width / 2, DISPLAY_HEIGHT / 2 - BulbOff_Height / 2, BulbOff_Width, BulbOff_Height, BulbOff);
+      display.display();
+      delay(500);
+      display.clear();
+      display.drawXbm(DISPLAY_WIDTH / 2 - BulbOn_Width / 2, DISPLAY_HEIGHT / 2 - BulbOn_Height / 2, BulbOn_Width, BulbOn_Height, BulbOn);
+      break;
+    case 4:
+      display.drawXbm(DISPLAY_WIDTH / 2 - ThumbsUp_Width / 2, DISPLAY_HEIGHT / 2 - ThumbsUp_Height / 2, ThumbsUp_Width, ThumbsUp_Height, ThumbsUp);
+      break;
+    case 5:
+      display.drawXbm(DISPLAY_WIDTH / 2 - CheckSign_Width / 2, DISPLAY_HEIGHT / 2 - CheckSign_Height / 2, CheckSign_Width, CheckSign_Height, CheckSign);
+      break;
+    case 6:
+      display.drawXbm(DISPLAY_WIDTH / 2 - Stones_width / 2, DISPLAY_HEIGHT / 2 - Stones_height / 2, Stones_width, Stones_height, Stones);
+      break;
+    case 7:
+      display.drawXbm(DISPLAY_WIDTH / 2 - Loriot_Width / 2, DISPLAY_HEIGHT / 2 - Loriot_Height / 2, Loriot_Width, Loriot_Height, Loriot);
+      break;
+    default:
+      display.drawXbm(DISPLAY_WIDTH / 2 - Snoopy_Width / 2, DISPLAY_HEIGHT / 2 - Snoopy_Height / 2, Snoopy_Width, Snoopy_Height, Snoopy);
+      break;
+  }
+  display.display();
+  delay(2500);
+
+  display.clear();
+  display.drawString(64, 16, "Versuche letztes");
+  display.drawString(64, 32, "WLAN...");
+  display.display();
+  delay(1000);
+
+  WiFiManager wifiManager;
+  //reset saved settings
+  //wifiManager.resetSettings();
+
+  wifiManager.setAPCallback(configModeCallback);
+
+  if (!wifiManager.autoConnect("AndyWetter")) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+
+
+  display.clear();
+  display.drawXbm(DISPLAY_WIDTH / 2 - WiFi_Logo_Width / 2, DISPLAY_HEIGHT / 2 - WiFi_Logo_Height / 2 - 5, WiFi_Logo_Width, WiFi_Logo_Height, WiFi_Logo);
+  display.drawString(64, WiFi_Logo_Height + 10, WiFi.SSID());
+  display.display();
+  delay(2000);
+
+  ui.setTargetFPS(30);
+/*
+  // You can change this to
+  // TOP, LEFT, BOTTOM, RIGHT
+  ui.setIndicatorPosition(BOTTOM);
+
+  // Defines where the first frame is located in the bar.
+  ui.setIndicatorDirection(LEFT_RIGHT);
+
+  // You can change the transition that is used
+  // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_TOP, SLIDE_DOWN
+  ui.setFrameAnimation(SLIDE_LEFT);
+*/
+  // Add frames
+  ui.disableIndicator();
+
+  ui.setFrames(frames, numberOfFrames);
+
+  // Inital UI takes care of initalising the display too.
+  ui.init();
+
+  Serial.println("");
+
+  updateData(&display);
+
+  ticker.attach(UPDATE_INTERVAL_SECS, setReadyForWeatherUpdate);
+
+  calcPress();
+  LastPress = millis();
+
+}
+
+void loop() {
+
+  if (readyForUpdate && ui.getUiState()->frameState == FIXED) {
+    updateData(&display);
+  }
+
+  if (ui.getUiState()->frameState == FIXED) {
+    // lockaler Druck etc. alle 10sec
+    if ( (millis() - LastPress) >= 10000) {
+      calcPress();
+      LastPress = millis();
+    }
+    AnimationDirection AnimationDir = (AnimationDirection)random(0, 4);
+    ui.setFrameAnimation(AnimationDir);
+  }
+
+  int remainingTimeBudget = ui.update();
+
+  if (remainingTimeBudget > 0) {
+    // You can do some work here
+    // Don't do stuff if you are below your
+    // time budget.
+    //Serial.print("Rem. Time Budget: ");
+    //Serial.println(remainingTimeBudget);
+    delay(remainingTimeBudget);
+  }
+}
+
+
